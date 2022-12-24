93ebc7773e13d7e801ac9a0c14dd5dac09ca7589
Emil Sayahi <97276123+emmyoh@users.noreply.github.com>
2022-12-24 03:18:31 -0500

    Minor tweaks
    - Add favicon to documentation
    - Remove `cargo-features` from `Cargo.toml`

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,10 @@
 
 #![cfg_attr(feature = "dox", feature(doc_cfg))]
 #![allow(clippy::needless_doctest_main)]
+#![doc(
+	html_logo_url = "https://github.com/Dirout/vaultist/raw/master/branding/icon.png",
+	html_favicon_url = "https://github.com/Dirout/vaultist/raw/master/branding/icon.png"
+)]
 #![feature(drain_filter)]
 #![feature(slice_partition_dedup)]
 
